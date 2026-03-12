e2f23510b238336ac660bd20f0b36a54e073f064
wlsdzyzl <wlsdzyzl@gmail.com>
2026-03-12 22:00:46 +0800

    Add bash script and resources for t-SNE visualization

---
+++ resources/tsne/draw_tsne.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+train_flemme --config /home/guoqingzhang/meshage/resources/tsne/train_tsne_medsdf_gen_bladder.yaml
+train_flemme --config /home/guoqingzhang/meshage/resources/tsne/train_tsne_medsdf_gen_brain.yaml
+train_flemme --config /home/guoqingzhang/meshage/resources/tsne/train_tsne_medsdf_gen_colon.yaml
+train_flemme --config /home/guoqingzhang/meshage/resources/tsne/train_tsne_medsdf_gen_duodenum.yaml
+train_flemme --config /home/guoqingzhang/meshage/resources/tsne/train_tsne_medsdf_gen_gallbladder.yaml
+train_flemme --config /home/guoqingzhang/meshage/resources/tsne/train_tsne_medsdf_gen_lca.yaml
+train_flemme --config /home/guoqingzhang/meshage/resources/tsne/train_tsne_medsdf_gen_rca.yaml
+train_flemme --config /home/guoqingzhang/meshage/resources/tsne/train_tsne_medsdf_gen_liver.yaml
+train_flemme --config /home/guoqingzhang/meshage/resources/tsne/train_tsne_medsdf_gen_pancreas.yaml
+train_flemme --config /home/guoqingzhang/meshage/resources/tsne/train_tsne_medsdf_gen_skull.yaml
+train_flemme --config /home/guoqingzhang/meshage/resources/tsne/train_tsne_medsdf_gen_spleen.yaml
+train_flemme --config /home/guoqingzhang/meshage/resources/tsne/train_tsne_medsdf_gen_stomach.yaml
+train_flemme --config /home/guoqingzhang/meshage/resources/tsne/train_tsne_medsdf_gen_trachea.yaml
+train_flemme --config /home/guoqingzhang/meshage/resources/tsne/train_tsne_medsdf_gen_uterus.yaml
+
+
+
+# test_flemme --config /home/guoqingzhang/meshage/resources/tsne/tsne_medsdf_gen_bladder.yaml
+# test_flemme --config /home/guoqingzhang/meshage/resources/tsne/tsne_medsdf_gen_brain.yaml
+# test_flemme --config /home/guoqingzhang/meshage/resources/tsne/tsne_medsdf_gen_colon.yaml
+# test_flemme --config /home/guoqingzhang/meshage/resources/tsne/tsne_medsdf_gen_duodenum.yaml
+# test_flemme --config /home/guoqingzhang/meshage/resources/tsne/tsne_medsdf_gen_gallbladder.yaml
+# test_flemme --config /home/guoqingzhang/meshage/resources/tsne/tsne_medsdf_gen_lca.yaml
+# test_flemme --config /home/guoqingzhang/meshage/resources/tsne/tsne_medsdf_gen_rca.yaml
+# test_flemme --config /home/guoqingzhang/meshage/resources/tsne/tsne_medsdf_gen_liver.yaml
+# test_flemme --config /home/guoqingzhang/meshage/resources/tsne/tsne_medsdf_gen_pancreas.yaml
+# test_flemme --config /home/guoqingzhang/meshage/resources/tsne/tsne_medsdf_gen_skull.yaml
+# test_flemme --config /home/guoqingzhang/meshage/resources/tsne/tsne_medsdf_gen_spleen.yaml
+# test_flemme --config /home/guoqingzhang/meshage/resources/tsne/tsne_medsdf_gen_stomach.yaml
+# test_flemme --config /home/guoqingzhang/meshage/resources/tsne/tsne_medsdf_gen_trachea.yaml
+# test_flemme --config /home/guoqingzhang/meshage/resources/tsne/tsne_medsdf_gen_uterus.yaml
+
